663574ca7504d688b466fff040c6a4c3f6da3fdd
sandeep18p <sandeep.pansari98@gmail.com>
2024-04-30 22:20:41 +0530

    one

--- main.js
+++ main.js
@@ -2,8 +2,53 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// Define the routes and implement the CRUD operations for the books collection
+let currentId = 1;
+const data = [{ id: currentId, title: 'Harry potter', author: 'Tom', publicationYear: 2024 }];
 
+app.get('books',(req,res)=>{
+ res.status(200).json(data);
+})
+
+
+app.get('/books/:id',(req,res)=>{
+  const book = data.find(b => b.id === parseInt(req.params.id));
+  if (book) {
+    res.status(200).json(book);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+})
+
+app.post('books',(req,res)=>{
+   const newData = {
+    id : currentId++,
+    title : req.body.title,
+    author : req.body.author,
+    publicationYear :req.body.publicationYear
+   }
+   data.push(newData);
+   res.status(201).json(newData);
+})
+
+app.put('/books/:id',(req,res)=>{
+  const book = data.find(b => b.id === parseInt(req.params.id));
+  if (book) {
+    Object.assign(book, req.body);
+    res.status(200).json(book);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+})
+
+app.delete('/books/:id', (req, res) => {
+  const bookIndex = data.findIndex(b => b.id === parseInt(req.params.id));
+  if (bookIndex !== -1) {
+    const [deletedBook] = data.splice(bookIndex, 1);
+    res.status(200).json(deletedBook);
+  } else {
+    res.status(404).json({ error: 'Book not found' });
+  }
+});
 const server = app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
